2225f4b90a34cdfdabfa6eca70ce03b0a6007f8c
AlexRailstone <96865953+AlexRailstone@users.noreply.github.com>
2022-08-25 11:08:28 +0300

    commit Default Changelist

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -31,6 +31,7 @@ public class Main {
             System.out.println(us);
         }
 
+        userService.removeUserById(3);
         userService.cleanUsersTable();
         userService.dropUsersTable();
     }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,8 +1,14 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.util.List;
+
+import static jm.task.core.jdbc.util.Util.getSessionFactory;
 
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
@@ -12,31 +18,101 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        Session session = getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        String sql = "CREATE TABLE IF NOT EXISTS users " +
+                "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                "name VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, " +
+                "age TINYINT NOT NULL)";
+
+        Query query = session.createSQLQuery(sql).addEntity(User.class);
+
+        query.executeUpdate();
+        transaction.commit();
+        session.close();
 
     }
 
     @Override
     public void dropUsersTable() {
+        try(Session session = getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            String sql = "DROP TABLE IF EXISTS users";
+
+            Query query = session.createSQLQuery(sql).addEntity(User.class);
+
+            query.executeUpdate();
+            transaction.commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
+        try (Session session = getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student object
+            User user = new User();
+            user.setName(name);
+            user.setLastName(lastName);
+            user.setAge(age);
+            session.save(user);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void removeUserById(long id) {
+        try( Session session = getSessionFactory().openSession();) {
+            Transaction transaction = session.beginTransaction();
+            String sql = "DELETE FROM USERS WHERE ID=?";
+
+            Query query = session.createSQLQuery(sql).addEntity(User.class);
+
+            query.executeUpdate();
+            transaction.commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            return session.createQuery("from User", User.class).list();
+        }
     }
 
     @Override
     public void cleanUsersTable() {
+        Session session = getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        String sql = "DELETE FROM USERS";
+
+        Query query = session.createSQLQuery(sql).addEntity(User.class);
+
+        query.executeUpdate();
+        transaction.commit();
+        session.close();
 
     }
 }

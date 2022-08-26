2ba536b657776c01c60dbadedad6eab986ea4623
Alexandra Timoshchenko <96865953+AlexandraTimoshchenko@users.noreply.github.com>
2022-08-26 10:55:57 +0300

    Update UserServiceImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -7,7 +7,7 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl  implements UserService {
-    // private UserDaoJDBCImpl userDaoJBDC = NEW UserDaoJDBCImpl();
+    // private UserDaoJDBCImpl userDaoJBDC = new UserDaoJDBCImpl();
     private UserDaoHibernateImpl userDaoJDBC = new UserDaoHibernateImpl();
     public void createUsersTable() {
         userDaoJDBC.createUsersTable();
